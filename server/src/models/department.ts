8288eeced5355328b9d0f3675d629b4e12af9e8d
Yufeng Li <yufengli206@gmail.com>
2025-09-23 13:06:31 -0400

    [fix]: updated department model

--- server/src/models/department.ts
+++ server/src/models/department.ts
@@ -3,15 +3,15 @@ import { Schema, model, Document } from "mongoose";
 export interface IDepartment extends Document {
   collegeName: string;
   departmentName: string;
-  supervisor: string;
-  admin: boolean;
+  supervisor?: string;
+  admin?: string;
 }
 
 const DepartmentSchema = new Schema<IDepartment>({
   collegeName: { type: String, required: true },
   departmentName: { type: String, required: true },
-  supervisor: { type: String, required: true },
-  admin: { type: Boolean, default: false },
+  supervisor: String,
+  admin: String,
 });
 
 export const Department = model<IDepartment>("Department", DepartmentSchema);
