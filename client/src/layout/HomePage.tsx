70cf42fd30b0e9c68ea1fae016967b6b35896013
Chenjing Lin <clin7@umbc.edu>
2025-10-27 21:33:22 -0400

    [feat] improved UI of all screens

--- client/src/layout/HomePage.tsx
+++ client/src/layout/HomePage.tsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
-import { Typography, Box, Container, Grid } from "@mui/material";
+import {Box, Container, Grid } from "@mui/material";
 import type { GridColDef } from "@mui/x-data-grid";
 
-import EmployeeForm from "../components/Employee/createEmployee";
-import Display from "../components/Employee/Display";
 import EmployeeTable from "../components/Employee/EmployeeTable";
 import TemporaryDrawer from "../components/Employee/Drawer";
 import VisaStatsComponent from "../components/Reports/visaSummary";
 import type { EmployeeItem } from "../api";
+import { calculateDaysLeft } from "../util";
 
 export default function HomePage() {
-  const [reload] = useState<number>(0);
-
+  
   const employeeColumns: GridColDef<EmployeeItem>[] = [
     { field: "employeeId", headerName: "Employee ID", width: 130 },
     { field: "firstName", headerName: "First Name", width: 150 },
@@ -30,7 +28,39 @@ export default function HomePage() {
         return date ? new Date(date).toLocaleDateString("en-US") : "N/A";
       }
     },
-    { field: "daysRemain", headerName: "Days Remain", width: 120 },
+    { field: "daysRemain", headerName: "Days Remain", width: 120,
+      valueGetter: (_, row) => {
+        const days = row.visaHistory[0]?.expireDate
+          ? calculateDaysLeft(row.visaHistory[0].expireDate)
+          : "-";
+          return days ? days : "N/A";
+     },
+      renderCell: (params) => {
+        const days = params.value
+
+        let color =  "/img_src/status_red.png";
+        if (days !== "N/A" && days < 30) color = "/img_src/status_red.png";
+        else if (days < 60) color =  "/img_src/status_orange.png";              
+        else if (days >= 90) color =  "/img_src/status_green.png";
+
+        return (
+          <Box
+            sx={{
+            backgroundImage: `url(${color})`,
+            color: "white",
+            textAlign: "center",
+            width: 80,
+            height: 40,
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            }}
+          >
+            {days}
+          </Box>
+        );
+      },
+    },
   ];
   return (
     <Box sx={{ ml: 7 }}>
@@ -40,12 +70,12 @@ export default function HomePage() {
         </Box>
 
         <Grid container spacing={2} display={"flex"} justifyContent={"space-around"}>
-          <Grid size={{ xs: 12, lg: 8 }} sx={{ mr: 5 }}>
+          <Grid size={{ xs: 12, lg: 8 }} sx={{ mr: 5, cursor: "pointer"}}>
             <EmployeeTable
               title="Current Live Cases"
               url="employee/getEmployee"
               columns={employeeColumns}
-              reload={reload}
+              initialSort="daysRemain"
             />
           </Grid>
 
@@ -55,15 +85,6 @@ export default function HomePage() {
         </Grid>
       </Container>
     </Box>
-
-    //     <Dialog open={isCreateDialogOpen} onClose={handleCloseCreateDialog} maxWidth="md" fullWidth>
-    //       <DialogContent>
-    //         <EmployeeForm onClose={handleCloseCreateDialog}/>
-    //       </DialogContent>
-    //     </Dialog>
-
-
-
   );
 }
 
--- client/src/layout/ManageEmployee.tsx
+++ client/src/layout/ManageEmployee.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Typography, Box, Container, Grid, Button } from "@mui/material";
+import {Box, Container, Grid, Button } from "@mui/material";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import type { GridColDef } from "@mui/x-data-grid";
@@ -7,15 +7,94 @@ import type { GridColDef } from "@mui/x-data-grid";
 import EmployeeForm from "../components/Employee/createEmployee";
 import EmployeeTable from "../components/Employee/EmployeeTable";
 import TemporaryDrawer from "../components/Employee/Drawer";
-import Display from "../components/Employee/Display";
+import type { EmployeeItem } from "../api";
+import { calculateDaysLeft } from "../util";
 
 export default function ManageEmployee() {
   const [isCreateDialogOpen, setIsCreateDialogOpen] = useState<boolean>(false);
-  const [reload, setReload] = useState<number>(0);
-  const triggerReload = () => {
-    setReload(prev => prev + 1);
-    console.log("reloaded");
+  const employeeColumns: GridColDef<EmployeeItem>[] = [
+    { field: "employeeId", headerName: "Employee ID", width: 130 },
+    { field: "firstName", headerName: "First Name", width: 150 },
+    { field: "lastName", headerName: "Last Name", width: 150 },
+    {
+      field: "college", headerName: "college", width: 150,
+      valueGetter: (_, row) => {
+        const college = row.departmentInfo?.college;
+        return college ? college : "N/A";
+      }
+    },
+    {
+      field: "department", headerName: "department", width: 150,
+      valueGetter: (_, row) => {
+        const department = row.departmentInfo?.department;
+        return department ? department : "N/A";
+      }
+    },
+    {
+      field: "visaType", headerName: "Visa Type", width: 150,
+      valueGetter: (_, row) => {
+        const visaType = row.visaHistory[0]?.visaType;
+        return visaType ? visaType : "N/A";
+      }
+    },
+    {
+      field: "expireDate", headerName: "Exp Date", width: 150,
+      valueGetter: (_, row) => {
+        const date = row.visaHistory[0]?.expireDate;
+        return date ? new Date(date).toLocaleDateString("en-US") : "N/A";
+      }
+    },
+    { field: "daysRemain", headerName: "Days Remain", width: 120,
+      valueGetter: (_, row) => {
+        const days = row.visaHistory[0]?.expireDate
+          ? calculateDaysLeft(row.visaHistory[0].expireDate)
+          : "-";
+          return days ? days : "N/A";
+     },
+      renderCell: (params) => {
+        const days = params.value
 
+        let color =  "/img_src/status_red.png";
+        if (days !== "N/A" && days < 30) color = "/img_src/status_red.png";
+        else if (days < 60) color =  "/img_src/status_orange.png";              
+        else if (days >= 90) color =  "/img_src/status_green.png";
+
+        return (
+          <Box
+            sx={{
+            backgroundImage: `url(${color})`,
+            color: "white",
+            textAlign: "center",
+            width: 80,
+            height: 40,
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            }}
+          >
+            {days}
+          </Box>
+        );
+      },
+    },
+    {
+      field: "archive",
+      headerName: "",
+      width: 120,
+      renderCell: () => (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={(event) => {event.stopPropagation();}}
+        >
+          Archive
+        </Button>
+      ),
+    },
+  ];
+
+  const triggerReload = () => {
+    console.log("reloaded");
   };
   const handleOpenCreateDialog = () => {
     setIsCreateDialogOpen(true);
@@ -26,21 +105,40 @@ export default function ManageEmployee() {
   };
 
   return (
+    <Box sx={{ ml: 7 }}>
     <Container>
-      {/*Left sidebar */}
-      <TemporaryDrawer />
-      <Display />
-      <Grid container>
-        <Button variant="contained" color="primary" onClick={handleOpenCreateDialog}>
-          Create Employee
-        </Button>
-      </Grid>
+        <Box sx={{ mb: 2, mt: 2 }}>
+          <TemporaryDrawer />
+        </Box>
+        <Box  sx={{position: "relative"}}>
+          <Grid size={{ xs: 12, lg: 8 }} sx={{ mr: 5, cursor: "pointer"}}>
+            <EmployeeTable
+              title="All Live Cases"
+              url="employee/getEmployee"
+              columns={employeeColumns}
+              initialSort="employeeID"
+            />
+          </Grid>
+          <Grid container     
+            sx={{
+              position: "absolute",
+              top: 15,
+              right: 90,
+              zIndex: 10,     
+            }}>
+            <Button variant="contained" color="primary" onClick={handleOpenCreateDialog}>
+              Create Employee
+            </Button>
+          </Grid>
+          </Box>
+
       <Dialog open={isCreateDialogOpen} onClose={handleCloseCreateDialog} maxWidth="md" fullWidth>
         <DialogContent>
           <EmployeeForm onClose={handleCloseCreateDialog} onAddSuccess={triggerReload} />
         </DialogContent>
       </Dialog>
     </Container>
+    </Box>
 
 
 
